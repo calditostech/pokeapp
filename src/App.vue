4af5367dc6d5ada9887aeadfc0fd8dd29d740b0e
Ricardo Moreira <calditos1988@outlook.com>
2021-03-11 18:02:24 -0300

    criando os filtros

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
-    <div v-for="(poke,index) in pokemons" :key="index">
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/pokemon-logo-png.png">
+      <hr>
+      <h4 class="is-size-4">Pokemon App</h4>
+      <input type="text" name="" id="" placeholder="Buscar Pokemon" v-model="busca">
+      <button class="btn-buscar" @click="buscar">Buscar</button>
+    <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
       <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
     </div>
   </div>
 </template>
@@ -13,17 +20,41 @@ export default {
   name: 'App',
   data(){
     return {
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
       console.log("Pegou a lista de pokemons");
       this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
     })
   },
   components:{
     Pokemon
+  },
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+        if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+        }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+        }
+    }
+  },
+  computed:{
+    /*
+    resultadoBusca: function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
+    */
   }
 }
 </script>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,12 +1,41 @@
 <template>
-   <div>
-       <h1>{{name | upper}}</h1>
-       <small>{{url}}</small>
+     <div id="pokemon">
+     <div class="w3-container">
+          <h2>Pokemon Card</h2>
+      <div class="w3-card-4" style="width:50%">
+           <img :src="pokemon.front" alt="Placeholder imagem">
+      <div class="w3-container w3-center">
+           <p>{{num}} - {{name | upper}}</p>
+           <p>{{pokemon.type}}</p>
+      </div>
+      </div>
+      <div class="content">
+            <button class="button is-medium is fullwidth" @click="exibirValores">Caracteristicas</button>
+      </div>
+   </div>
    </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+    created: function(){
+        axios.get(this.url).then(res => {
+           this.pokemon.type = res.data.types[0].type.name;
+           this.pokemon.front = res.data.sprites.front_default;
+           this.pokemon.back = res.data.sprites.back_default;
+           console.log(this.pokemon);
+        })
+    },
+    data(){
+        return {
+            pokemon: {
+                type: '',
+                front: '',
+                back: ''
+            }
+        }
+    },
     props: {
         num: Number,
         name: String,
@@ -17,10 +46,17 @@ export default {
             var newName = value[0].toUpperCase() + value.slice(1);
             return newName;
         }
+    },
+    methods: {
+        exibirValores: function(){
+
+        }
     }
 }
 </script>
 
 <style>
-
+    #pokemon{
+        margin-top: 2%;
+    }
 </style>
